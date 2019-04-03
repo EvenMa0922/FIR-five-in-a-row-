efec0119760ed2608f227470643083fd508e0394
Yusen <mayusen0922@gmail.com>
2019-04-03 13:07:33 -0700

    ready to deploy

---
+++ build/precache-manifest.0a9c362d7d4563a3bc94044ccccd7c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3a1f6c91a22ae06c11a6",
+    "url": "/static/js/main.4ade3fcb.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "3a1f6c91a22ae06c11a6",
+    "url": "/static/css/main.4080c6a3.chunk.css"
+  },
+  {
+    "revision": "7668a90fca5685d3e95c6c2f6645a13f",
+    "url": "/index.html"
+  }
+];
